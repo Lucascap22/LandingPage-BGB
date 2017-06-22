ab4a1562806b20b6ddd75addaf4a8faa293de603
Lucas Costa <lucas.cap@live.com>
2017-06-22 18:47:48 -0300

    LandingPage BGB

---
+++ index.js
@@ -0,0 +1,45 @@
+//Script âncoras
+var buttonHome = document.querySelector("header button.home");
+var buttonJogos = document.querySelector("header button.jogos");
+var buttonFinancie = document.querySelector("header button.financie");
+var buttonContato = document.querySelector("header button.contato");
+
+adicionarEventoAncora(buttonHome, "banner");
+adicionarEventoAncora(buttonJogos, "exemplos");
+adicionarEventoAncora(buttonFinancie, "etapas");
+adicionarEventoAncora(buttonContato, "footer");
+
+function adicionarEventoAncora(button, idPosicao) {
+    button.addEventListener("click", function(){
+        document.getElementById(idPosicao).scrollIntoView();
+    });
+}
+
+
+
+//Script contador
+  var target_date = new Date("08/07/2017").getTime();
+  var dias, horas, minutos, segundos;
+  var regressiva = document.getElementById("regressiva");
+
+  setInterval(function () {
+
+    var current_date = new Date().getTime();
+    var segundos_f = (target_date - current_date) / 1000;
+
+    dias = parseInt(segundos_f / 86400);
+    segundos_f = segundos_f % 86400;
+
+    horas = parseInt(segundos_f / 3600);
+    segundos_f = segundos_f % 3600;
+
+    minutos = parseInt(segundos_f / 60);
+    segundos = parseInt(segundos_f % 60);
+
+    document.getElementById('dia').innerHTML = dias;
+    document.getElementById('hora').innerHTML = horas;
+    document.getElementById('minuto').innerHTML = minutos;
+    document.getElementById('segundo').innerHTML = segundos;
+
+
+  }, 1000);
